42cd1e1936e4d8a622cc9ed7ec9faf37c50180c8
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2025-10-31 00:29:29 -0400

    Starting to fix pieces

--- src/containerl/interface/__init__.py
+++ src/containerl/interface/__init__.py
@@ -1,10 +1,69 @@
-from containerl.interface.proto_pb2_grpc import EnvironmentServiceServicer as EnvironmentService, AgentServiceServicer as AgentService, EnvironmentServiceStub, AgentServiceStub
-from containerl.interface.proto_pb2 import InitRequest, ResetRequest, StepRequest, RenderResponse, SpacesResponse, Empty, ResetResponse, StepResponse, ObservationRequest, ActionResponse, EnvironmentType, Space
-from containerl.interface.proto_pb2_grpc import add_EnvironmentServiceServicer_to_server, add_AgentServiceServicer_to_server
-from containerl.interface.utils import native_to_numpy_space, numpy_to_native_space, space_proto_to_json, json_to_space_proto, generate_spaces_info_from_gym_spaces, numpy_to_native, native_to_numpy
-from containerl.interface.environment.client import EnvironmentClient
 from containerl.interface.agent.client import AgentClient
-from containerl.interface.environment.server_factory import create_environment_server
 from containerl.interface.agent.server_factory import create_agent_server
+from containerl.interface.environment.client import EnvironmentClient
+from containerl.interface.environment.server_factory import create_environment_server
+from containerl.interface.proto_pb2 import (
+    ActionResponse,
+    Empty,
+    EnvironmentType,
+    InitRequest,
+    ObservationRequest,
+    RenderResponse,
+    ResetRequest,
+    ResetResponse,
+    Space,
+    SpacesResponse,
+    StepRequest,
+    StepResponse,
+)
+from containerl.interface.proto_pb2_grpc import AgentServiceServicer as AgentService
+from containerl.interface.proto_pb2_grpc import (
+    AgentServiceStub,
+    EnvironmentServiceStub,
+    add_AgentServiceServicer_to_server,
+    add_EnvironmentServiceServicer_to_server,
+)
+from containerl.interface.proto_pb2_grpc import (
+    EnvironmentServiceServicer as EnvironmentService,
+)
+from containerl.interface.utils import (
+    generate_spaces_info_from_gym_spaces,
+    json_to_space_proto,
+    native_to_numpy,
+    native_to_numpy_space,
+    numpy_to_native,
+    numpy_to_native_space,
+    space_proto_to_json,
+)
 
-__all__ = ["EnvironmentClient", "AgentClient", "EnvironmentService", "AgentService", "EnvironmentServiceStub", "AgentServiceStub", "InitRequest", "ResetRequest", "StepRequest", "RenderResponse", "SpacesResponse", "Empty", "ResetResponse", "StepResponse", "ObservationRequest", "ActionResponse", "create_environment_server", "add_EnvironmentServiceServicer_to_server", "create_agent_server", "add_AgentServiceServicer_to_server", "EnvironmentType", "Space", "native_to_numpy_space", "numpy_to_native_space", "space_proto_to_json", "json_to_space_proto", "generate_spaces_info_from_gym_spaces", "numpy_to_native", "native_to_numpy"]
+__all__ = [
+    "EnvironmentClient",
+    "AgentClient",
+    "EnvironmentService",
+    "AgentService",
+    "EnvironmentServiceStub",
+    "AgentServiceStub",
+    "InitRequest",
+    "ResetRequest",
+    "StepRequest",
+    "RenderResponse",
+    "SpacesResponse",
+    "Empty",
+    "ResetResponse",
+    "StepResponse",
+    "ObservationRequest",
+    "ActionResponse",
+    "create_environment_server",
+    "add_EnvironmentServiceServicer_to_server",
+    "create_agent_server",
+    "add_AgentServiceServicer_to_server",
+    "EnvironmentType",
+    "Space",
+    "native_to_numpy_space",
+    "numpy_to_native_space",
+    "space_proto_to_json",
+    "json_to_space_proto",
+    "generate_spaces_info_from_gym_spaces",
+    "numpy_to_native",
+    "native_to_numpy",
+]
