fa8d79fd40f1f611ac588f58fd39fee60dd8da32
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2025-12-21 15:47:18 -0500

    Refactored envs test

--- tests/integration/test_environment_build_run_test.py
+++ tests/integration/test_environment_build_run_test.py
@@ -1,6 +1,8 @@
 """Tests for building, running, and validating generic environment builds."""
 
 import logging
+import os
+from typing import Any
 
 import pytest
 
@@ -11,71 +13,117 @@ from containerl import (
 )
 from containerl.cli import build_run, stop_container
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+root_directory = os.path.join(current_directory, "../..")
+
 pytestmark = pytest.mark.integration
 
 # Define environments and their build configurations
+ENVS_BASE_FOLDER = "examples/environments/"
 TEST_CASES = [
-    # pytest.param(
-    #    "./examples/anylogic/stock_management/environment/",
-    #    id="anylogic/stock_management",
-    # ),
     pytest.param(
-        "./examples/generic/environments/discrete_actions/",
+        "./generic/discrete_actions/",
+        {},
         id="generic/discrete_actions",
     ),
     pytest.param(
-        "./examples/generic/environments/multibinary_actions/",
+        "./generic/multibinary_actions/",
+        {},
         id="generic/multibinary_actions",
     ),
     pytest.param(
-        "./examples/generic/environments/multidiscrete_actions/",
+        "./generic/multidiscrete_actions/",
+        {},
         id="generic/multidiscrete_actions",
     ),
     pytest.param(
-        "./examples/generic/environments/continuous_actions/",
+        "./generic/continuous_actions/",
+        {},
         id="generic/continuous_actions",
     ),
     pytest.param(
-        "./examples/generic/environments/continuous_action/",
+        "./generic/continuous_action/",
+        {},
         id="generic/continuous_action",
     ),
     pytest.param(
-        "./examples/generic/environments/discrete_actions_vectorized/",
+        "./generic/discrete_actions_vectorized/",
+        {},
         id="generic/discrete_actions_vectorized",
     ),
     pytest.param(
-        "./examples/generic/environments/multibinary_actions_vectorized/",
+        "./generic/multibinary_actions_vectorized/",
+        {},
         id="generic/multibinary_actions_vectorized",
     ),
     pytest.param(
-        "./examples/generic/environments/multidiscrete_actions_vectorized/",
+        "./generic/multidiscrete_actions_vectorized/",
+        {},
         id="generic/multidiscrete_actions_vectorized",
     ),
     pytest.param(
-        "./examples/generic/environments/continuous_actions_vectorized/",
+        "./generic/continuous_actions_vectorized/",
+        {},
         id="generic/continuous_actions_vectorized",
     ),
     pytest.param(
-        "./examples/generic/environments/continuous_action_vectorized/",
+        "./generic/continuous_action_vectorized/",
+        {},
         id="generic/continuous_action_vectorized",
+    ),
+    pytest.param(
+        "./gymnasium/classic_control/",
+        {
+            "render_mode": None,
+            "env_name": "CartPole-v1",
+        },
+        id="gymnasium/classic_control",
+    ),
+    pytest.param(
+        "./gymnasium/box2d/",
+        {
+            "render_mode": "rgb_array",
+            "env_name": "LunarLander-v3",
+        },
+        id="gymnasium/box2d",
+    ),
+    pytest.param(
+        "./gymnasium/mujoco/",
+        {
+            "render_mode": "rgb_array",
+            "env_name": "Ant-v5",
+        },
+        id="gymnasium/mujoco",
+    ),
+    pytest.param(
+        "./gymnasium/atari/",
+        {
+            "render_mode": None,
+            "env_name": "ALE/Breakout-v5",
+            "obs_type": "ram",
+        },
+        id="gymnasium/atari",
     ),
 ]
 
 
-@pytest.mark.parametrize("env_folder", TEST_CASES)
-def test_build_run_environment(env_folder: str) -> None:
+@pytest.mark.parametrize("env_folder, init_args", TEST_CASES)
+def test_build_run_environment(env_folder: str, init_args: dict[str, Any]) -> None:
     """Test building, running, and validating a generic environment build."""
     # Run the build_run_test command
-    image, _ = build_run(env_folder)
     logger = logging.getLogger(__name__)
+    image, addresses = build_run(
+        os.path.join(root_directory, ENVS_BASE_FOLDER),
+        entrypoint_args=[f"{env_folder}/environment.py"],
+    )
 
     success = False
     try:
         if "vectorized" in env_folder:
-            vec_environment_check("localhost:50051")
+            vec_environment_check(addresses[0], **init_args)
         else:
-            environment_check("localhost:50051")
-            gym_environment_check("localhost:50051")
+            environment_check(addresses[0], **init_args)
+            gym_environment_check(addresses[0], **init_args)
         success = True
     except Exception as e:
         logger.error(f"Error testing environment connection: {str(e)}")
